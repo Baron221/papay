c326a646595400d24f3e9b27d75525bc5c94342a
Baron221 <baronjon080@gmail.com>
2023-05-25 21:43:15 +0900

    feat:loyihani express qismini shakllantrib olish

---
+++ server.js
@@ -0,0 +1,21 @@
+const http =require("http");
+const mongodb =require("mongodb");
+
+let db;
+const connectionString ="mongodb+srv://baronjon080:YQ6VI7K73QOkvlVe@cluster0.mabua11.mongodb.net/Reja?retryWrites=true&w=majority"
+
+mongodb.connect(connectionString,{useNewUrlParser:true, 
+    useUnifiedTopology:true,
+},(err,client)=> {
+    if(err) console.log("ERROR on connection MongoDB");
+    else{
+        console.log("MongoDB connection succeed");
+        module.exports = client;
+        const app =require("./app");
+        const server = http.createServer(app);
+        let PORT = 3003;
+        server.listen(PORT , function(){
+            console.log(`The server is running succesfully on port : ${PORT} , http://localhost:${PORT}`)
+        });
+    }    
+});    
