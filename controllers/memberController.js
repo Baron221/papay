918f356e2c2b97d9e42cbfc0123c4214bf5378ee
Baron221 <baronjon080@gmail.com>
2023-06-01 16:13:44 +0900

    feat: member login process development

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -1,13 +1,13 @@
-const Member = require("../models/member");
+const Member = require("../models/Member");
 
 let memberController =  module.exports;
 
 memberController.signup = async (req,res) => {
     try{  
       console.log("POST:cont/signup");
-      const data =req.body;
-      const member =new Member();
-      const new_member = await member.signupData(data);
+      const data =req.body,
+      member =new Member(),
+      new_member = await member.signupData(data);
       res.json({state:'succeed',data: new_member})
   }catch(err){
       console.log(`ERROR , cont/signup ${err.message}` );
@@ -15,16 +15,20 @@ memberController.signup = async (req,res) => {
   
   }}
 
-memberController.login = (req,res) => {
-    console.log("POST cont.home");
+memberController.login = async (req,res) => {
+    console.log("POST:cont/login");
+    const data =req.body,
+      member =new Member(),
+      result = await member.loginData(data);
+    
+    res.json({state:'succeed',data: result});
 
-    res.send("login sahifasidasiz")
 };
 
 memberController.logout = (req,res) => {
-    console.log("GET cont.home");
+    console.log(`ERROR , cont/signup ${err.message}` );
+    res.json({state:'fail', message:err.message });
 
-    res.send("logout sahifasidasiz")
 };
 
 
