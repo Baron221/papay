bfbaf426404aad4fe425e496b9f3cfbf1f71f7e3
Baron221 <baronjon080@gmail.com>
2023-06-02 23:52:40 +0900

    feat:ejs framework orqali frontendni qurishni boshlash

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -1,40 +1,35 @@
 const Member = require("../models/Member");
 
-let memberController =  module.exports;
+let memberController = module.exports;
 
-memberController.signup = async (req,res) => {
-    try{  
-      console.log("POST:cont/signup");
-      const data =req.body,
-      member =new Member(),
+memberController.signup = async (req, res) => {
+  try {
+    console.log("POST:cont/signup");
+    const data = req.body,
+      member = new Member(),
       new_member = await member.signupData(data);
-      res.json({state:'succeed',data: new_member})
-  }catch(err){
-      console.log(`ERROR , cont/signup ${err.message}` );
-      res.json({state:'fail', message:err.message });
-  
-  }}
+    res.json({ state: "succeed", data: new_member });
+  } catch (err) {
+    console.log(`ERROR , cont/signup ${err.message}`);
+    res.json({ state: "fail", message: err.message });
+  }
+};
 
-memberController.login = async (req,res) => {
-    try{console.log("POST:cont/login");
-    const data =req.body,
-      member =new Member(),
+memberController.login = async (req, res) => {
+  try {
+    console.log("POST:cont/login");
+    const data = req.body,
+      member = new Member(),
       result = await member.loginData(data);
-    
-    res.json({state:'succeed',data: result});
-}catch(err){
+
+    res.json({ state: "succeed", data: result });
+  } catch (err) {
     console.log(`ERROR, cont/login, ${err.message}`);
-    res.json({state:"fail",message:err.message})
-}
-
+    res.json({ state: "fail", message: err.message });
+  }
 };
 
-
-
-memberController.logout = (req,res) => {
-    console.log(`ERROR , cont/signup ${err.message}` );
-    res.json({state:'fail', message:err.message });
-
+memberController.logout = (req, res) => {
+  console.log("GET cont.logout");
+  res.send("logout sahifasidasiz");
 };
-
-
