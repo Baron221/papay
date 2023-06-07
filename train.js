5fd86560caf991d02fe7c1c120640bd07ce42d66
Baron221 <baronjon080@gmail.com>
2023-06-07 15:59:29 +0900

    feat:develop Product Schema model to work with database

--- train.js
+++ train.js
@@ -1,17 +1,22 @@
-const prices =[2,1,30,3,10];
-console.log("You should buy stock =>",buyStock(prices));
 
-function buyStock(stock_prices){
-    let maxProf = 0;
-    let minPrice = stock_prices[0];
-    for (let sell = 1; sell < stock_prices.length; sell++){
-        let sellPrice = stock_prices[sell]
-        let profit =  sellPrice - minPrice
-        maxProf = Math.max(maxProf, profit)
 
-        if(sellPrice < minPrice) minPrice = sellPrice;
+// function buyStock(stock_prices){
+//     let maxProf = 0;
+//     let minPrice = stock_prices[0];
+//     for (let sell = 1; sell < stock_prices.length; sell++){
+//         let sellPrice = stock_prices[sell]
+//         let profit =  sellPrice - minPrice
+//         maxProf = Math.max(maxProf, profit)
+
+//         if(sellPrice < minPrice) minPrice = sellPrice;
     
-    }
-    return `index: ${stock_prices.length} , profit: ${maxProf}`
-}
+//     }
+//     return `index: ${stock_prices.length} , profit: ${maxProf}`
+// }
 
+function findDigits(str){
+    const stringNum = str.replace(/\D/g, ''); 
+    return stringNum;
+   }
+   console.log(findDigits('ad5we34jkf89'));
+   
