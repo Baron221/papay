0a89fd37967d5926a2dcd31ac547327a33b05c9b
Baron221 <baronjon080@gmail.com>
2023-06-10 01:42:10 +0900

    feat:restaurant user getting restaurant related products

--- models/Product.js
+++ models/Product.js
@@ -3,11 +3,23 @@ const ProductModel = require("../schema/product.model");
 const assert = require("assert");
 const Definer = require("../lib/mistake");
 
-
 class Product {
   constructor() {
     this.productModel = ProductModel;
   }
+  async getAllProductsDataResto(member) {
+    try {
+      member._id = shapeIntoMongooseObjectId(member._id);
+      const result = await this.productModel.find({
+        restaurant_mb_id: member._id,
+      });
+      assert.ok(result, Definer.general_err1);
+      return result;
+    } catch (err) {
+      throw err;
+    }
+  }
+
   async addNewProductData(data, member) {
     try {
       data.restaurant_mb_id = shapeIntoMongooseObjectId(member._id);
