2744b670a9d6bdaab6485eff20e6c36a9da10012
Baron221 <baronjon080@gmail.com>
2023-07-17 01:30:37 +0900

    feat:develp geetChosenMember api business logic part two

--- models/member.js
+++ models/member.js
@@ -27,7 +27,7 @@ class Member {
     } catch (err) {
       throw err;
     }
-  };
+  }
 
   async loginData(input) {
     try {
@@ -50,24 +50,23 @@ class Member {
     } catch (err) {
       throw err;
     }
-  };
+  }
 
-  async getChosenMemberData(member , id) {
+  async getChosenMemberData(member, id) {
     try {
       id = shapeIntoMongooseObjectId(id);
-      
+
       console.log("member:::", member);
 
-      if(member){
-        // condition if not seen before
+      if (member) {
+        await this.viewChosenItemByMember(member, id, "member");
       }
 
       const result = await this.memberModel
         .aggregate([
           { $match: { _id: id, mb_status: "ACTIVE" } },
-          {$unset:"mb_password"},
-                      
-      ])
+          { $unset: "mb_password" },
+        ])
         .exec();
 
       assert.ok(result, Definer.general_err2);
@@ -76,6 +75,29 @@ class Member {
       throw err;
     }
   }
-};
+
+  async viewChosenItemByMember(member, view_ref_id, group_type) {
+    try {
+      view_ref_id = shapeIntoMongooseObjectId(view_ref_id);
+      const mb_id = shapeIntoMongooseObjectId(member._id);
+
+      const view = new View(mb_id);
+      const isValid = await view.validateChosenTarget(view_ref_id, group_type);
+      assert.ok(isValid, Definer.general_err2);
+      
+      const doesExist =await view.checkViewExistence(view_ref_id)
+      console.log("doesExist:", doesExist);
+
+      if(!doesExist){
+        const result = await view.insertMemberView(view_ref_id,group_type);
+        assert.ok(result ,Definer.general_err1)
+      }
+
+      return true;
+    } catch (err) {
+      throw err;
+    }
+  }
+}
 
 module.exports = Member;
